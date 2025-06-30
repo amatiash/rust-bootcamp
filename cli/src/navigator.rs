a8f67dcc2a7e08f86e0f1a505de5a95e8b1992ef
Anton Matiash <amatiash.dev@gmail.com>
2025-06-30 05:13:16 +0400

    Remove last page on epic and story deletion

--- cli/src/navigator.rs
+++ cli/src/navigator.rs
@@ -69,6 +69,10 @@ impl Navigator {
                     self.db
                         .delete_epic(epic_id)
                         .with_context(|| anyhow!("Failed to delete epic"))?;
+
+                    if !self.pages.is_empty() {
+                        self.pages.pop();
+                    }
                 }
             }
             Action::CreateStory { epic_id } => {
@@ -89,6 +93,10 @@ impl Navigator {
                     self.db
                         .delete_story(epic_id, story_id)
                         .with_context(|| anyhow!("Failed to delete story"))?;
+
+                    if !self.pages.is_empty() {
+                        self.pages.pop();
+                    }
                 }
             }
             Action::Exit => {
