efd4087444795ec647b9f85406cbc99e6ab1ece4
Anton Matiash <amatiash.dev@gmail.com>
2025-07-07 19:23:28 +0400

    api-axum: Added template for Step 2 of Stage 3

--- api-axum/src/handlers/mod.rs
+++ api-axum/src/handlers/mod.rs
@@ -1,11 +1,15 @@
 mod handlers_inner;
 
-use crate::models::*;
-use axum::{Json, response::IntoResponse};
+use crate::{models::*, AppState};
+use axum::{Json, response::IntoResponse, extract::State};
 
 // ---- CRUD for Questions ----
 
-pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
+pub async fn create_question(
+    // Example of how to add state to a route. Note that we are using ".." to ignore the other fields in AppState.
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Json(question): Json<Question>,
+) -> impl IntoResponse {
     let question = QuestionDetail {
         question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
         title: question.title,
@@ -16,7 +20,8 @@ pub async fn create_question(Json(question): Json<Question>) -> impl IntoRespons
     Json(question)
 }
 
-pub async fn read_questions() -> impl IntoResponse {
+pub async fn read_questions(// TODO: add questions_dao from app state as an argument
+) -> impl IntoResponse {
     let question = QuestionDetail {
         question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
         title: "Newly Created Question".to_string(),
@@ -27,13 +32,20 @@ pub async fn read_questions() -> impl IntoResponse {
     Json(vec![question])
 }
 
-pub async fn delete_question(Json(_question_uuid): Json<QuestionId>) {
-    // To be implemented
+pub async fn delete_question(
+    // TODO: add questions_dao from app state as an argument
+    Json(question_uuid): Json<QuestionId>,
+) {
+    // ...
 }
 
 // ---- CRUD for Answers ----
 
-pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
+pub async fn create_answer(
+    // Example of how to add state to a route
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(answer): Json<Answer>,
+) -> impl IntoResponse {
     let answer = AnswerDetail {
         answer_uuid: "a1a14a9c-ab9e-481b-8120-67f675531ed2".to_string(),
         question_uuid: answer.question_uuid,
@@ -44,7 +56,10 @@ pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
     Json(answer)
 }
 
-pub async fn read_answers(Json(_question_uuid): Json<QuestionId>) -> impl IntoResponse {
+pub async fn read_answers(
+    // TODO: add answers_dao from app state as an argument
+    Json(question_uuid): Json<QuestionId>,
+) -> impl IntoResponse {
     let answer = AnswerDetail {
         answer_uuid: "a1a14a9c-ab9e-481b-8120-67f675531ed2".to_string(),
         question_uuid: "b068cd2f-edac-479e-98f1-c5f91008dcbd".to_string(),
@@ -55,6 +70,9 @@ pub async fn read_answers(Json(_question_uuid): Json<QuestionId>) -> impl IntoRe
     Json(vec![answer])
 }
 
-pub async fn delete_answer(Json(_answer_uuid): Json<AnswerId>) {
-    // To be implemented
+pub async fn delete_answer(
+    // TODO: add answers_dao from app state as an argument
+    Json(answer_uuid): Json<AnswerId>,
+) {
+    // ...
 }
