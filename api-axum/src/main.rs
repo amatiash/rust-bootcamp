111b60bed77521712a0b77d4a2d18c43b9d8b2ad
Anton Matiash <amatiash.dev@gmail.com>
2025-07-07 17:05:23 +0400

    api-axum: Implemented Step 1.1 of Stage 2

--- api-axum/src/main.rs
+++ api-axum/src/main.rs
@@ -1,8 +1,9 @@
-// TODO: import log, pretty_env_logger, dotenv, and PgPoolOptions
+use log::info;
+use sqlx::postgres::PgPoolOptions;
 
 use axum::{
-    routing::{delete, get, post},
     Router,
+    routing::{delete, get, post},
 };
 
 mod handlers;
@@ -12,23 +13,24 @@ use handlers::*;
 
 #[tokio::main]
 async fn main() {
-    // TODO: Initialize pretty_env_logger
-    // TODO: Initialize dotenv
+    pretty_env_logger::init();
+    dotenvy::dotenv().ok();
 
-    // Create a new PgPoolOptions instance with a maximum of 5 connections.
-    // Use dotenv to get the database url.
-    // Use the `unwrap` or `expect` method instead of handling errors. If an
-    // error occurs at this stage the server should be terminated.
-    // See examples on GitHub page: https://github.com/launchbadge/sqlx
-    // let pool = todo!();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    // Using slqx, execute a SQL query that selects all questions from the questions table.
-    // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
-    // make sure we can connect to the database.
-    // let recs = todo!();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to database");
+
+    let recs = sqlx::query("SELECT * FROM questions")
+        .fetch_all(&pool)
+        .await
+        .expect("Failed to fetch questions");
 
     info!("********* Question Records *********");
-    // TODO: Log recs with debug formatting using the info! macro
+    info!("{:?}", recs);
 
     let app = Router::new()
         .route("/question", post(create_question))
