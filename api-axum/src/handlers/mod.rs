b7c9e18663ae2cd16e47012c531e2195d1746d84
Anton Matiash <amatiash.dev@gmail.com>
2025-07-07 16:01:06 +0400

    api-axum: Implemented Step 2 of Stage 1

--- api-axum/src/handlers/mod.rs
+++ api-axum/src/handlers/mod.rs
@@ -1,33 +1,58 @@
 use crate::models::*;
-use axum::{response::IntoResponse, Json};
+use axum::{Json, response::IntoResponse};
 
 // ---- CRUD for Questions ----
 
 pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-    todo!()
+    let question = QuestionDetail {
+        question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
+        title: question.title,
+        description: question.description,
+        created_at: "2022-12-31 18:44:08.287442".to_string(),
+    };
+
+    Json(question)
 }
 
 pub async fn read_questions() -> impl IntoResponse {
-    todo!()
+    let question = QuestionDetail {
+        question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
+        title: "Newly Created Question".to_string(),
+        description: "My Description".to_string(),
+        created_at: "2022-12-31 18:44:08.287442".to_string(),
+    };
+
+    Json(vec![question])
 }
 
-pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-    todo!()
+pub async fn delete_question(Json(_question_uuid): Json<QuestionId>) {
+    // To be implemented
 }
 
 // ---- CRUD for Answers ----
 
-// TODO: Create a POST route to /answer which accepts an `Answer` and returns `AnswerDetail` as JSON.
-//       The handler function should be called `create_answer`.
-//
-//       hint: this function should look very similar to the create_question function above
+pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
+    let answer = AnswerDetail {
+        answer_uuid: "a1a14a9c-ab9e-481b-8120-67f675531ed2".to_string(),
+        question_uuid: answer.question_uuid,
+        content: answer.content,
+        created_at: "2022-12-31 13:11:59.728682".to_string(),
+    };
 
-// TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
-//       The handler function should be called `read_answers`.
-//
-//       hint: this function should look very similar to the read_questions function above
+    Json(answer)
+}
 
-// TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
-//       The handler function should be called `delete_answer`.
-//
-//       hint: this function should look very similar to the delete_question function above
+pub async fn read_answers(Json(_question_uuid): Json<QuestionId>) -> impl IntoResponse {
+    let answer = AnswerDetail {
+        answer_uuid: "a1a14a9c-ab9e-481b-8120-67f675531ed2".to_string(),
+        question_uuid: "b068cd2f-edac-479e-98f1-c5f91008dcbd".to_string(),
+        content: "test question".to_string(),
+        created_at: "2022-12-31 13:11:59.728682".to_string(),
+    };
+
+    Json(vec![answer])
+}
+
+pub async fn delete_answer(Json(_answer_uuid): Json<AnswerId>) {
+    // To be implemented
+}
