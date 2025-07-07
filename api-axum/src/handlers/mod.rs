1336a368496a75cd95607764c34cf1ddaa5c8d4b
Anton Matiash <amatiash.dev@gmail.com>
2025-07-07 19:32:43 +0400

    api-axum: Added template for Step 3 of Stage 3

--- api-axum/src/handlers/mod.rs
+++ api-axum/src/handlers/mod.rs
@@ -1,15 +1,31 @@
 mod handlers_inner;
 
 use crate::{AppState, models::*};
-use axum::{Json, extract::State, response::IntoResponse};
+use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
+
+impl IntoResponse for handlers_inner::HandlerError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            handlers_inner::HandlerError::BadRequest(msg) => {
+                (StatusCode::BAD_REQUEST, msg).into_response()
+            }
+            handlers_inner::HandlerError::InternalError(msg) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response()
+            }
+        }
+    }
+}
 
 // ---- CRUD for Questions ----
 
 pub async fn create_question(
-    // Example of how to add state to a route. Note that we are using ".." to ignore the other fields in AppState.
     State(AppState { questions_dao, .. }): State<AppState>,
     Json(question): Json<Question>,
 ) -> impl IntoResponse {
+    // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+    // TODO: Replace the fake data below with a call to `handlers_inner::create_question`.
+    // Return the result wrapped in JSON in the success case and an `HandlerError` in the error case.
+    // NOTE: `IntoResponse` is implemented for `HandlerError` above.
     let question = QuestionDetail {
         question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
         title: question.title,
@@ -23,6 +39,10 @@ pub async fn create_question(
 pub async fn read_questions(
     State(AppState { questions_dao, .. }): State<AppState>,
 ) -> impl IntoResponse {
+    // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+    // TODO: Replace the fake data below with a call to `handlers_inner::read_questions`.
+    // Return the result wrapped in JSON in the success case and an `HandlerError` in the error case.
+    // NOTE: `IntoResponse` is implemented for `HandlerError` above.
     let question = QuestionDetail {
         question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
         title: "Newly Created Question".to_string(),
@@ -37,16 +57,22 @@ pub async fn delete_question(
     State(AppState { questions_dao, .. }): State<AppState>,
     Json(question_uuid): Json<QuestionId>,
 ) {
-    // ...
+    // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+    // TODO: Make a call to `handlers_inner::delete_question`.
+    // Return a unit type in the success case and an `HandlerError` in the error case.
+    // NOTE: `IntoResponse` is implemented for `HandlerError` above.
 }
 
 // ---- CRUD for Answers ----
 
 pub async fn create_answer(
-    // Example of how to add state to a route
     State(AppState { answers_dao, .. }): State<AppState>,
     Json(answer): Json<Answer>,
 ) -> impl IntoResponse {
+    // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+    // TODO: Replace the fake data below with a call to `handlers_inner::create_answer`.
+    // Return the result wrapped in JSON in the success case and an `HandlerError` in the error case.
+    // NOTE: `IntoResponse` is implemented for `HandlerError` above.
     let answer = AnswerDetail {
         answer_uuid: "a1a14a9c-ab9e-481b-8120-67f675531ed2".to_string(),
         question_uuid: answer.question_uuid,
@@ -61,6 +87,10 @@ pub async fn read_answers(
     State(AppState { answers_dao, .. }): State<AppState>,
     Json(question_uuid): Json<QuestionId>,
 ) -> impl IntoResponse {
+    // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+    // TODO: Replace the fake data below with a call to `handlers_inner::read_answers`.
+    // Return the result wrapped in JSON in the success case and an `HandlerError` in the error case.
+    // NOTE: `IntoResponse` is implemented for `HandlerError` above.
     let answer = AnswerDetail {
         answer_uuid: "a1a14a9c-ab9e-481b-8120-67f675531ed2".to_string(),
         question_uuid: "b068cd2f-edac-479e-98f1-c5f91008dcbd".to_string(),
@@ -75,5 +105,8 @@ pub async fn delete_answer(
     State(AppState { answers_dao, .. }): State<AppState>,
     Json(answer_uuid): Json<AnswerId>,
 ) {
-    // ...
+    // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+    // TODO: Make a call to `handlers_inner::delete_answer`.
+    // Return a unit type in the success case and an `HandlerError` in the error case.
+    // NOTE: `IntoResponse` is implemented for `HandlerError` above.
 }
